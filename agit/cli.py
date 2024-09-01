60387fa958befb23add59a3782335ef902296d4f
Aman Kumar <amanrajkumar4@gmail.com>
2024-09-02 01:13:44 +0530

    Add basic structure for init command

--- agit/cli.py
+++ agit/cli.py
@@ -1,2 +1,20 @@
+import argparse
+
 def main():
-    print("Hello World!")
+    args = parse_args()
+    args.func(args)
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='A simple git clone')
+
+    commands = parser.add_subparsers(title='commands', dest='command')
+    commands.required = True
+
+    init_parser = commands.add_parser('init', help='Initialize a new, empty repository')
+    init_parser.set_defaults(func=init)
+    
+    return parser.parse_args()
+
+def init(args):
+    print("Hello World from AGit")
+    print('Initialized an empty repository')
