6c758a6d086b886fe23a28538b60eb5c3d68fd42
Aman Kumar <amanrajkumar4@gmail.com>
2024-09-03 01:33:00 +0530

    cat-file: display hased objects

--- agit/cli.py
+++ agit/cli.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import sys
 
 from . import data
 
@@ -19,6 +20,10 @@ def parse_args():
     hash_object_parser = commands.add_parser('hash-object', help='Store a given blob in the repository')
     hash_object_parser.add_argument('file')
     hash_object_parser.set_defaults(func=hash_object)
+
+    cat_file_parser = commands.add_parser('cat-file', help='Provide content of repository objects')
+    cat_file_parser.add_argument('object')
+    cat_file_parser.set_defaults(func=cat_file)
     
     return parser.parse_args()
 
@@ -29,3 +34,7 @@ def init(args):
 def hash_object(args):
     with open(args.file, 'rb') as f:
         print(data.hash_object(f.read()))
+
+def cat_file(args):
+    sys.stdout.flush()
+    sys.stdout.buffer.write(data.get_object(args.object))
--- agit/data.py
+++ agit/data.py
@@ -12,3 +12,7 @@ def hash_object(data):
     with open(f'{GIT_DIR}/objects/{oid}', 'wb') as out:
         out.write(data)
     return oid
+
+def get_object(oid):
+    with open(f'{GIT_DIR}/objects/{oid}', 'rb') as f:
+        return f.read()
