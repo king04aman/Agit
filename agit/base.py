b5e38679620716fb27760971c1f006a1875c3f7e
Aman Kumar <amanrajkumar4@gmail.com>
2024-09-07 22:40:05 +0530

    add: remove existing stuff when reading tree (read-tree) to avoid conflicts
    This is done so that we won't have any older files left around after a read-tree called.
    Before this change, if we save a tree A which contains only a.txt, then we save tree B which contains a.txt and b.txt and then we read-tree A, we will have b.txt left over in working directory.

--- agit/base.py
+++ agit/base.py
@@ -44,7 +44,25 @@ def get_tree(oid, base_path=''):
             assert False, f'Unknown tree entry {type_}'
     return result
 
+def _empty_current_directory():
+    for root, dirnames, filenames in os.walk('.', topdown=False):
+        for filename in filenames:
+            path = os.path.relpath(f'{root}/{filename}')
+            if is_ignored(path) or not os.path.isfile(path):
+                continue
+            os.remove(path)
+        for dirname in dirnames:
+            path = os.path.relpath(f'{root}/{dirname}')
+            if is_ignored(path):
+                continue
+            try:
+                os.rmdir(path)
+            except (FileNotFoundError, OSError):
+                # Deletion might fail if the directory is not empty
+                pass
+
 def read_tree(tree_oid):
+    _empty_current_directory()
     for path, oid in get_tree(tree_oid, base_path='./').items():
         os.makedirs(os.path.dirname(path), exist_ok=True)
         with open(path, 'wb') as f:
