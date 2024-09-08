d7019e5723a637c4deb850127faff4d923dc14fe
Aman Kumar <amanrajkumar4@gmail.com>
2024-09-08 22:56:07 +0530

    add: set parent to HEAD

--- agit/base.py
+++ agit/base.py
@@ -70,6 +70,11 @@ def read_tree(tree_oid):
 
 def commit(message):
     commit = f'tree {write_tree()}\n'
+
+    HEAD = data.get_HEAD()
+    if HEAD:
+        commit += f'parent {HEAD}\n'
+        
     commit += '\n'
     commit += f'{message}\n'
     oid = data.hash_object(commit.encode(), 'commit')
--- agit/data.py
+++ agit/data.py
@@ -11,6 +11,12 @@ def set_HEAD(oid):
     with open(f'{GIT_DIR}/HEAD', 'w') as f:
         f.write(oid)
 
+def get_HEAD():
+    if os.path.isfile(f'{GIT_DIR}/HEAD'):
+        with open(f'{GIT_DIR}/HEAD') as f:
+            return f.read().strip()
+    return None
+
 def hash_object(data, type_='blob'):
     obj = type_.encode() + b'\x00' + data
     oid = hashlib.sha1(obj).hexdigest()
