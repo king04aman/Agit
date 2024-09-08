62887c4035eddb491a9a38c03b81d9fef3259d9b
Aman Kumar <amanrajkumar4@gmail.com>
2024-09-08 23:02:30 +0530

    add: log implementation

--- agit/base.py
+++ agit/base.py
@@ -1,5 +1,8 @@
 import os
+import itertools
+import operator
 
+from collections import namedtuple
 from . import data
 
 def write_tree(directory='.'):
@@ -74,7 +77,7 @@ def commit(message):
     HEAD = data.get_HEAD()
     if HEAD:
         commit += f'parent {HEAD}\n'
-        
+
     commit += '\n'
     commit += f'{message}\n'
     oid = data.hash_object(commit.encode(), 'commit')
@@ -83,3 +86,22 @@ def commit(message):
 
 def is_ignored(path):
     return '.agit' in path.split('/')
+
+Commit = namedtuple('Commit', ['tree', 'parent', 'message'])
+
+def get_commit(oid):
+    parent = None
+
+    commit = data.get_object(oid, 'commit').decode()
+    lines = iter(commit.splitlines())
+    for line in itertools.takewhile(operator.truth, lines):
+        key, value = line.split(' ', 1)
+        if key == 'tree':
+            tree = value
+        elif key == 'parent':
+            parent = value
+        else:
+            assert False, f'Unknown field {key}'
+
+    message = '\n'.join(lines)
+    return Commit(tree=tree, parent=parent, message=message)
--- agit/cli.py
+++ agit/cli.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import sys
+import textwrap
 
 from . import base
 from . import data
@@ -37,6 +38,10 @@ def parse_args():
     commit_parser.add_argument('-m', '--message', required=True, help='Commit message')
     commit_parser.set_defaults(func=commit)
 
+    log_parser = commands.add_parser('log', help='Show history of the repository')
+    log_parser.set_defaults(func=log)
+    log_parser.add_argument('oid', nargs='?')
+
     return parser.parse_args()
 
 def init(args):
@@ -59,6 +64,15 @@ def read_tree(args):
 
 def commit(args):
     print(base.commit(args.message))
+
+def log(args):
+    oid = args.oid or data.get_HEAD()
+    while oid:
+        commit = base.get_commit(oid)
+        print(f'commit {oid}\n')
+        print(textwrap.indent(commit.message, '    '))
+        print('')
+        oid = commit.parent
 
 if __name__ == '__main__':
     main()
