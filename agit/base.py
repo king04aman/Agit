b239f30114e58d8237c4f124020dd8070026a38a
Aman Kumar <amanrajkumar4@gmail.com>
2024-09-10 01:26:30 +0530

    base: try different directories when searching for a ref

--- agit/base.py
+++ agit/base.py
@@ -1,6 +1,7 @@
 import os
 import itertools
 import operator
+import string
 
 from collections import namedtuple
 from . import data
@@ -113,8 +114,22 @@ def get_commit(oid):
     return Commit(tree=tree, parent=parent, message=message)
 
 def get_oid(name):
-    return data.get_ref(name) or name
+    refs_to_try = [
+        f'{name}',
+        f'refs/{name}',
+        f'refs/tags/{name}',
+        f'refs/heads/{name}',
+    ]
+
+    for ref in refs_to_try:
+        if data.get_ref(ref):
+            return data.get_ref(ref)
+    
+    is_hex = all(c in string.hexdigits for c in name)
+    if len(name) == 40 and is_hex:
+        return name
+    
+    assert False, f'Unknown name {name}'
 
 def is_ignored(path):
     return '.agit' in path.split('/')
-
