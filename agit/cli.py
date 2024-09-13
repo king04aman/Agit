15a0555f0590e62623be3a986877f64a1ca012fb
Aman Kumar <amanrajkumar4@gmail.com>
2024-09-14 00:26:00 +0530

    feat: created reverse lookup dictionary to show refs that point to each commit

--- agit/cli.py
+++ agit/cli.py
@@ -119,9 +119,15 @@ def commit(args):
 
 def log(args):
     """Display the commit history starting from the specified object ID."""
+    refs = {}
+    for refname, ref in data.iter_refs():
+        refs.setdefault(ref.value, []).append(refname)
+
     for oid in base.iter_commits_and_parents({args.oid}):
         commit_data = base.get_commit(oid)
-        print(f'commit {oid}\n')
+
+        refs_str = f' ({", ".join(refs[oid])})' if oid in refs else ''
+        print(f'commit {oid}{refs_str}')
         print(textwrap.indent(commit_data.message, '    '))
         print('')
 
