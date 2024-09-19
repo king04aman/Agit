6c65ef2ca8383748f7dcf468d1a29075dfdda780
Aman Kumar <amanrajkumar4@gmail.com>
2024-09-19 13:55:07 +0530

    refator: retrieve remote refs in a separate function

--- agit/remote.py
+++ agit/remote.py
@@ -4,8 +4,13 @@ from . import data
 def fetch(remote_path):
     """Fetch objects and refs from a remote repository."""
     print("Will fetch the following refs:")
+    for refname, _ in _get_remote_refs(remote_path, 'refs/heads').items():
+        print(f'- {refname}')
+
+
+def _get_remote_refs(remote_path, prefix=''):
+    """Retrieve refs from a remote repository."""
     with data.change_git_dir(remote_path):
-        for refname, _ in data.iter_refs('refs/heads'):
-            print(f'- {refname}')
-            data.update_ref(refname, data.get_ref(refname))
+        return {refname: ref.value for refname, ref in data.iter_refs(prefix)}
+
 
